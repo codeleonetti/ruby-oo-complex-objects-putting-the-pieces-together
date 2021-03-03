f0832b554c06fb3f30eeb4883ca5447f4c3ee9a7
john <john.d.leonetti@gmail.com>
2021-03-02 19:02:04 -0800

    commit tree 2d30ede991de925fdba6b0f6d864e592cc43296d

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,26 @@
 # Make your shoe class here!
+require "pry"
+class Shoe
+    attr_accessor :color, :size, :material, :condition, :cobble
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def brand
+        @brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        
+     self.condition = "new"
+    end
+
+    # def condition
+    #     @condition
+    # end
+
+end
+
+
